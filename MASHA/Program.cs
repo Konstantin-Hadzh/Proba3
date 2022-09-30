b40e21779a415b75fde2162c48990dd32b3e78fa
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-09-30 22:24:18 +0300

    MASHA com

---
+++ MASHA/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, Маша. А Женя дома?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
--- ProgSum/Program.cs
+++ ProgSum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3970;
-int numberB = 796376;
+﻿int numberA = new Random().Next(1, 10); // случайное число от 1 до 9
+Console.WriteLine(numberA); 
+int numberB = new Random().Next(1, 10); //случайное число от 1 до 9
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
 
